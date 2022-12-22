b29cbb639b80aa0a9e01904389f065f2fc7f3748
ebelabrie <aelowe1@asu.edu>
2022-12-22 10:41:33 -0700

    paradise awaits

--- src/api/generate-random-airport.js
+++ src/api/generate-random-airport.js
@@ -1,16 +1,16 @@
-const { exec } = require('child_process');
+// const { exec } = require('child_process');
 
-async function generateRandomAirport() {
-    try {
-        // run the Python function and get the result
-        const response = await exec('python3 ./generator.py');
-         // return the result as a JSON
-        const data = await response.json();
-        return data;
-      } catch (error) {
-        console.error(error);
-      }
+// async function generateRandomAirport() {
+//     try {
+//         // run the Python function and get the result
+//         const response = await exec('python3 ./generator.py');
+//          // return the result as a JSON
+//         const data = await response.json();
+//         return data;
+//       } catch (error) {
+//         console.error(error);
+//       }
 
-}
+// }
 
-export {generateRandomAirport}
+// export {generateRandomAirport}
--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-// expose the generateRandomAirport function as an API endpoint
-app.get('./api/generate-random-airport', async (req, res) => {
-    const result = await generateRandomAirport();
-    res.json(result);
-    console.log(result);
-  });
+// // expose the generateRandomAirport function as an API endpoint
+// app.get('./api/generate-random-airport', async (req, res) => {
+//     const result = await generateRandomAirport();
+//     res.json(result);
+//     console.log(result);
+//   });
 
-// start the server
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
-  });
+// // start the server
+// app.listen(3000, () => {
+//     console.log('Server listening on port 3000');
+//   });
