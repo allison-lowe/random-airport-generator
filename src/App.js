b17b15207baec00071c0032455934dfa5605a202
ebelabrie <aelowe1@asu.edu>
2022-12-22 01:22:34 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import dice from './dice.png';
+import axios from 'axios';
+
+// const airports = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Philadelphia', 'Phoenix', 'San Antonio', 'San Diego', 'Dallas', 'San Jose'];
 
 function App() {
+  const [airport, setAirport] = useState('New York');
+
+  async function getRandomAirport() {
+    // send an HTTP request to the API endpoint
+    const response = await axios.get('http://localhost:3000/api/generate-random-airport');
+    console.log(response);
+    // get the data from the response
+    const randomAirport = response.data;
+  
+    // use the data in your app
+    console.log(randomAirport);
+  }
+  // getRandomAirport();
+
+//   // use the useEffect hook to update the city when the image is clicked
+// useEffect(() => {
+// // choose a random city from the list
+// const newAirport = getRandomAirport();
+
+// // update the airport state
+// setAirport(newAirport);
+// }, []);
+
+const handleClick = () => {
+  // choose a random city from the list
+  const newAirport = getRandomAirport().then(response => response.json);
+
+  // update the airport state
+  setAirport(newAirport);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='App-header'>
+      {/* <img src={dice} className="App-logo" onClick={() => handleClick} /> */}
+        {<p onClick={handleClick}>
+          {airport}
+        </p>}
       </header>
     </div>
   );
---
+++ src/api/generator.py
@@ -0,0 +1,60 @@
+import random
+import requests
+from bs4 import BeautifulSoup
+
+
+class Generator:
+  def __main__():
+        
+    # URL for the Wikipedia page with a list of American Airlines destinations
+    url = 'https://en.wikipedia.org/wiki/List_of_American_Airlines_destinations'
+
+    # make a request to the Wikipedia page and parse the HTML
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # find the table with the list of airports
+    table = soup.find('table', {'class': 'wikitable sortable'})
+
+    # create a list of all US airports
+    airports = []
+    country = ''
+    airport = ''
+
+    # iterate over the rows in the table
+    for row in table.find_all('tr'):
+      # get the cells in the row
+      cells = row.find_all('td')
+      # if the row has cells, it is an airport row
+      if cells:
+        if (len(cells) > 4 and 
+            cells[3].text.strip() == ('Terminated' or 'Airport closed' or 'Seasonal') or 
+            cells[2].text.strip() == ('Terminated' or 'Airport closed' or 'Seasonal') or
+            cells[1].text.strip() == ('Terminated' or 'Airport closed' or 'Seasonal')):
+          continue
+        if (len(cells) == 5):
+          country = cells[0].text.strip()
+          city = cells[1].text.strip()
+          airport = cells[2].text.strip()
+        if (len(cells) == 4):
+          city = cells[0].text.strip()
+          airport = cells[1].text.strip()
+        if (len(cells) == 3):
+          airport = cells[0].text.strip()
+          
+        # add the airport to the list
+        airports.append({country: country, city: city, airport: airport})
+        # print(country + '\t' + '\t' + '\t' + city + '\t' + '\t' + airport) 
+        print(airport)
+
+    # generate a random airport city
+    def generate_airport_name():
+      # randomly select an airport from the list
+      airport = random.choice(airports)
+
+      # return the selected airport
+      return airport
+
+    # generate and print a random airport city
+    return generate_airport_name()
+    # print(generate_airport_name())
